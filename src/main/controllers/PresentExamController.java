951ae33141cfcdc3a0e6c58d44155bdf559047cb
Lisangely Goncalves <lisangelyg@gmail.com>
2024-01-29 17:07:25 -0400

    add search question

--- src/main/controllers/PresentExamController.java
+++ src/main/controllers/PresentExamController.java
@@ -1,5 +1,7 @@
 package main.controllers;
 
+import java.io.File;
+
 import main.models.Answers;
 import main.models.Questions;
 import main.models.Result;
@@ -7,10 +9,61 @@ import main.models.Exam;
 import javax.swing.*;
 
 public class PresentExamController {
-   /* private void setResult(Option){}
-    private void chooseExam(Exam_Name){}
+    //private void setResult(Option){}
+    //private void chooseExam(Exam_Name){}
     private void startExam(){}
     private Answers getAnswers(){}
-    private Questions getQuestions(){}*/
-    
+    private Questions getQuestions(){}
+
+    private String numberQuestion(String question){
+        
+        if (question != null && question.length() > 0) {
+            char lastCharacter = question.charAt(question.length() - 1);
+            if (Character.isDigit(lastCharacter)) {
+                int number = Character.getNumericValue(lastCharacter);
+                number++;
+                char newCharacter = Character.forDigit(number, 10);
+                return question.substring(0, question.length() - 1) + newCharacter; 
+            }
+        }
+        return question;
+    }
+
+    private void searchFile(String question, String nameFile, File[] files){
+        //llamada recursiva    
+        if (files != null) {
+                // Itera sobre los archivos para buscar el archivo
+                for (File file : files) {
+                    if (file.isFile() && file.getName().equals(question)) {
+                        // El archivo fue encontrado
+                        searchFile(numberQuestion(question), nameFile, files);
+                        return;
+                    }
+                }
+                // archivo no encontrado
+        }
+    }   
+
+    public void searchFolder() {
+        String directory = "/ruta/del/directorio";
+        //get para obtener nombre del examen
+        String nameFolder = "nombreCarpeta";
+        File searchedFolder = new File(directory);
+
+        if (searchedFolder.exists() && searchedFolder.isDirectory()) {
+            File[] files = searchedFolder.listFiles();
+
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory() && file.getName().equals(nameFolder)) {
+                        //carpeta encontrad
+                        String question = "Pregunta1";
+                        searchFile(question, nameFolder, files);
+                        return;
+                    }
+                }
+                // carpeta no encontrada
+           }    
+        }
+    }
 }
