0a49e62346c6ca9db052e7c206670cc5235b9e0f
Jesus <jesusecova73@gmail.com>
2024-01-31 21:13:33 -0400

    Rustic merge GaboBranch (CertificateView)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,19 @@
 package main;
 
+import main.views.pages.CertificateView;
 // import main.views.pages.ExamView;
 // import main.views.pages.LogInView;
-import main.views.pages.ExamsView;
+// import main.views.pages.ExamsView;
 // import main.views.pages.AdminExamsView;
 // import main.views.pages.RegisterUserView;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        new ExamsView();
+        // new ExamsView();
         // new AdminExamsView();
         // new LogInView();
         // new RegisterUserView();
         // new ExamView();
+        new CertificateView();
     }
 }
