4727840ec89aead10d343965820981b6957deb17
Sofia Marcano <soamaia03@gmail.com>
2024-02-03 12:49:08 -0400

    Add ExamEndedView

--- src/main/views/pages/ExamEndedView.java
+++ src/main/views/pages/ExamEndedView.java
@@ -1,10 +1,25 @@
 package main.views.pages;
 
 import main.views.pages.ResultView;
+import main.views.components.ExamMenu;
 import main.views.pages.ExamView;
+
+import java.awt.GridBagConstraints;
+
 import main.controllers.PresentExamController;
 
-public class ExamEndedView {
+public class ExamEndedView extends ExamView {
     //private void showScore(Result){}
     
+    protected void paintMenuPanel() {
+        menuPanel = new ExamMenu(questions.size());
+        
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.weighty = 0.5;
+        constraints.fill = GridBagConstraints.BOTH;
+
+        contentPanel.add(menuPanel, constraints);
+    }
 }
