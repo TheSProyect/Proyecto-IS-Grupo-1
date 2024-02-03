c6291fe1f710bf7fe721c0344a4cd9259ece5e35
PotatoMozzarella <148605894+TheSProyect@users.noreply.github.com>
2024-02-03 01:55:42 -0400

    Update EditProfileController.java

--- src/main/controllers/EditProfileController.java
+++ src/main/controllers/EditProfileController.java
@@ -1,8 +1,10 @@
 package main.controllers;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import main.models.UserData;
@@ -15,28 +17,45 @@ public class EditProfileController {
 
     }
 
-    public boolean setNewUsername(String newUsername){
+
+    public boolean setNewUsername(String newUsername) throws IOException{
         UserData currentUser = new UserData();
-		String directory = System.getProperty("user.dir");
-        directory = directory+File.separator+"src"+File.separator+"data"+File.separator+"Users"+File.separator+"Teachers"+File.separator+ newUsername +".txt";
-        try(BufferedReader adminReader = new BufferedReader(new FileReader(directory))){
-			return false;
-			
-		} catch (IOException e){
-            try(BufferedReader adminReader = new BufferedReader(new FileReader(directory))){
-            return true;
-            } catch (IOException e){
+        String directory = System.getProperty("user.dir");
+        if(currentUser.isAdmin()){
+		directory = System.getProperty("user.dir");
+        directory = directory+File.separator+"src"+File.separator;
+        directory = directory+"data"+File.separator+"Users"+File.separator;
+        directory = directory+"Teachers"+File.separator+ newUsername +".txt";
+        File adminfile = new File(directory);
+        
+        if(adminfile.exists()){
+            return false;
+        } else {
+            adminfile.createNewFile();
             currentUser.setUsername(newUsername);
             return true;
-            }
         }
         
+        } else {
+            directory = System.getProperty("user.dir");
+            directory = directory+File.separator+"src"+File.separator;
+            directory = directory+"data"+File.separator+"Users"+File.separator;
+            directory = directory+"Studens"+File.separator+ currentUser.getUsername() +".txt";
+            File userfile = new File(directory);
+            if(userfile.exists()){
+                return false;
+            } else { 
+                userfile.createNewFile();
+                currentUser.setUsername(newUsername);
+                return true;
+            }
+        }
 
     }
+
     public void setNewMail(String newMail){
         UserData currentUser = new UserData();
         currentUser.setMail(newMail);
-
     }
 
     public void setNewPassword(String newPassword){
@@ -45,4 +64,6 @@ public class EditProfileController {
 
     }
 
+    
 }
+
