baee1c3ad77f9407cc1045b94912bac6b28dcd6b
Jesus <jesusecova73@gmail.com>
2024-02-04 13:40:56 -0400

    Create PopUp classes

---
+++ src/main/views/components/PopUp.java
@@ -0,0 +1,40 @@
+package main.views.components;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import main.utils.Size;
+import main.views.pages.Frame;
+
+
+public class PopUp extends JFrame {
+    private static PopUp popup;
+    JPanel view;
+   
+    public static PopUp instance() {
+		if (popup == null){
+			popup = new PopUp();
+            popup.setLocationRelativeTo(Frame.instance());
+		}
+		return popup;
+	}
+
+    public PopUp() {
+        this.setUndecorated(true);
+        this.setVisible(true);
+        this.setMinimumSize(Size.instance().getPopupDimension());
+        this.setSize(Size.instance().getPopupDimension());
+    }
+
+    public void setView(JPanel view) {
+        if (this.view != null) {
+            this.remove(this.view);
+        }
+        this.view = view;
+        this.add(view);
+        
+        view.repaint();
+        this.pack();
+    }
+
+}
