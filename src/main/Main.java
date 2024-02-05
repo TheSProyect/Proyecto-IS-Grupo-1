2f4ea3f06be609cab5192d611d5d11fa1dbde155
ValCicco <valerria02@gmail.com>
2024-02-05 13:34:04 -0400

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,10 +7,7 @@ import main.views.pages.ExamsView;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        UserData.instance().setUsername("Usuario");
-        UserData.instance().setPassword("Contrasenia");
-
-        Frame.instance().setView(new ExamsView());
-        //Frame.instance().setView(new LogInView());
+        
+        Frame.instance().setView(new LogInView());
     }
 }
