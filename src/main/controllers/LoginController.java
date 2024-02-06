0c99f7bf7f8f8282b4f4c9a4ad1aac45b96ddca8
PotatoMozzarella <148605894+TheSProyect@users.noreply.github.com>
2024-02-06 01:22:55 -0400

    Update controllers

--- src/main/controllers/LoginController.java
+++ src/main/controllers/LoginController.java
@@ -12,7 +12,6 @@ public class LoginController {
 	public UserData currentUser = UserData.instance();
 	public LoginController(){
 		
-		
 	}
 	
 	public static void main(String[] args) throws IOException{
