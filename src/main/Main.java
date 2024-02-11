ff473717929106f2c60c23232cbaafa6cc2b7e28
Jesus <jesusecova73@gmail.com>
2024-02-10 21:40:52 -0400

    pinshe obag

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,15 @@
 package main;
 
 import main.utils.UserData;
-import main.views.pages.AdminExamsView;
 import main.views.pages.Frame;
-import main.views.pages.HelpView;
 import main.views.pages.LogInView;
-import main.views.pages.NewExamView;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         UserData.instance().setUsername("Usuario");
         UserData.instance().setPassword("Contrasenia");
 
-        // Frame.instance().setView(new LogInView());
-        Frame.instance().setView(ExamsView.instance());
+        Frame.instance().setView(new LogInView());
+        // Frame.instance().setView(ExamsView.instance());
     }
 }
--- src/main/views/components/Listing.java
+++ src/main/views/components/Listing.java
@@ -2,8 +2,6 @@ package main.views.components;
 
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.util.ArrayList;
 import java.util.List;
 import java.awt.BorderLayout;
@@ -26,115 +24,61 @@ import javax.swing.plaf.metal.MetalButtonUI;
 import main.utils.Palette;
 
 public class Listing extends JScrollPane{
+    JPanel listingPanel;
     List<JButton> listingButtons;
-    
+    String singleElement;
+    String textButton;
 
     JPanel titlePanel;
     JLabel title;
+    Button button;
     JButton createExam;
     JPanel titleButtonContainer;
 
-    public Listing(List<String> elementList, String textButton) {
+    public Listing(List<String> elements, String TextButton) {
         listingButtons = new ArrayList<JButton>();
-        System.out.print(elementList.size());
-        JPanel listContainer = new JPanel();
-        listContainer.setLayout(new BoxLayout(listContainer, BoxLayout.Y_AXIS));
-        listContainer.setBackground(Palette.instance().getWhite());
-
-        paintList(listContainer);
-        paintListElements(elementList, textButton, listContainer);
+        textButton = TextButton;
+       
+        paintListingPanel();
+        paintListElements(elements);
     }
 
     public Listing(List<String> questionList, List<String> answerList) {
-        JPanel listContainer = new JPanel();
-        listContainer.setLayout(new BoxLayout(listContainer, BoxLayout.Y_AXIS));
-        listContainer.setBackground(Palette.instance().getWhite());
-        
-
-        paintList(listContainer);
-        paintListElements(questionList, answerList, listContainer);
+        paintList();
+        paintListElements(questionList, answerList);
     }
 
-    
-    private void paintList(JPanel elementsList) {
-        
-        this.setViewportView(elementsList);
-        this.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        this.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    private void paintListingPanel() {
+        listingPanel = new JPanel();
+        listingPanel.setPreferredSize(new Dimension(860, 1500));
+        listingPanel.setBackground(Palette.instance().getWhite());
+
+        this.setViewportView(listingPanel);
+        this.setPreferredSize(new Dimension(250, 320));
         this.getVerticalScrollBar().setBackground(Palette.instance().getLightGray());
         changeScrollPaneLook();
-        
+
         Border border = BorderFactory.createLineBorder(Palette.instance().getWhite(), 3);
         this.setBorder(border);
-        
-    }
-    
-    private void paintListElements(List<String> questionList, List<String> answerList, JPanel listContainer) {
 
-        for (int i = 0; i < questionList.size(); i++) {
-            paintContent(questionList.get(i), answerList.get(i), listContainer);
-            
-        }
     }
-    
-    protected void paintListElements(List<String> elementList, String textButton,JPanel listContainer) {
-        
-        String singleElement;
-        for (int i = 0; i < elementList.size(); i++) {
-            singleElement = elementList.get(0);
+
+    protected void paintListElements(List<String> elements) {
+        JSeparator separator;
+        for (int i = 0; i < elements.size(); i++) {
+            singleElement = elements.get(i);
             
-            TitleButtonContainer(singleElement, textButton, listContainer);
+            titlePanel = new JPanel();
+            titlePanel.setBackground(Palette.instance().getWhite());
+            titlePanel.setPreferredSize(new Dimension(860, 60));
+            titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
             
+            listingPanel.add(titlePanel, BorderLayout.NORTH);
+            
+            TitleButtonContainer();
+            separator = paintTitleSeparator();
+            titlePanel.add(separator);
         }       
-    }
-    private void paintContent(String question, String answer, JPanel listContainer) {
-        JPanel listItem = new JPanel();
-        JPanel itemContainer = new JPanel();
-        JSeparator separator = paintTitleSeparator();
-
-        listItem.setBackground(Palette.instance().getWhite());
-        listItem.setMaximumSize(new Dimension(860, 860));
-        listItem.setLayout(new BoxLayout(listItem, BoxLayout.Y_AXIS));
-        listItem.setBorder(new EmptyBorder(0,  30,  0, 0));
-        
-
-        
-        itemContainer.setLayout(new BoxLayout(itemContainer, BoxLayout.Y_AXIS));
-        itemContainer.setBackground(Palette.instance().getWhite());
-        itemContainer.setBorder(new EmptyBorder(10,  10,  0, 5)); 
-
-        paintText(question, true, 19, 10, itemContainer);
-        
-        paintText(answer, false, 17, 15, itemContainer);
-        
-        
-        listItem.add(itemContainer);
-        listItem.add(separator);
-        
-        listContainer.add(listItem, BorderLayout.CENTER);
-        
-    }
-    private void TitleButtonContainer(String text, String textButton, JPanel listContainer) {
-        JSeparator separator = paintTitleSeparator();
-        
-        JPanel titleButtonContainer = new JPanel();
-        titleButtonContainer.setLayout(new BoxLayout(titleButtonContainer, BoxLayout.Y_AXIS));
-        titleButtonContainer.setBackground(Palette.instance().getWhite());
-
-
-        JPanel itemContainer = new JPanel();
-        itemContainer.setLayout(new BoxLayout(itemContainer, BoxLayout.X_AXIS));
-        itemContainer.setBackground(Palette.instance().getWhite());
-        itemContainer.setBorder(new EmptyBorder(10,  0,  10, 0)); 
-        paintText(textButton, false, 20, 0, itemContainer);
-
-        paintCreateButton(textButton, itemContainer);
-        
-        titleButtonContainer.add(itemContainer);
-
-        titleButtonContainer.add(separator);
-        titleButtonContainer.setBorder(new EmptyBorder(0,  40,  0, 40)); 
-        listContainer.add(titleButtonContainer);
     }
 
     protected JSeparator paintTitleSeparator() {
@@ -145,9 +89,31 @@ public class Listing extends JScrollPane{
         return line;
     }
 
-    
+    private void TitleButtonContainer() {
+        JPanel titleButtonContainer = new JPanel();
+        titleButtonContainer.setMaximumSize(new Dimension(1500, 58));
+        titleButtonContainer.setLayout(new BoxLayout(titleButtonContainer, BoxLayout.X_AXIS));
+        titleButtonContainer.setBackground(Palette.instance().getWhite());
 
-    private void paintCreateButton(String textButton,JPanel titleButtonContainer) {
+        paintTitleLabel(titleButtonContainer);
+
+        paintCreateButton(titleButtonContainer);
+        
+        titlePanel.add(titleButtonContainer);
+    }
+
+    protected void paintTitleLabel(JPanel titleButtonContainer) {
+        JLabel title = new JLabel();
+        title.setText(singleElement);
+        title.setFont(new Font("Nunito Sans", Font.ROMAN_BASELINE, 20));
+        title.setPreferredSize(new Dimension(944, 58));
+        title.setMaximumSize(new Dimension(2048, 58));
+        title.setVerticalAlignment(JLabel.BOTTOM);
+
+        titleButtonContainer.add(title, FlowLayout.LEFT);
+    }
+
+    private void paintCreateButton(JPanel titleButtonContainer) {
         JButton button = new JButton(textButton);
         button.setFont(new Font("Nunito Sans", Font.BOLD, 15));
         button.setForeground(Palette.instance().getWhite());
@@ -168,22 +134,67 @@ public class Listing extends JScrollPane{
 
 
 
-    
+    private void paintListElements(List<String> questionList, List<String> answerList) {
+        for (int i = 0; i < questionList.size(); i++) {
+            createPanel();
+            paintContent(questionList.get(i), answerList.get(i));
+            JSeparator separator = paintTitleSeparator();
+            titlePanel.add(separator);
+        }
+    }
+    protected void createPanel() {
+        titlePanel = new JPanel();
+        titlePanel.setBackground(Palette.instance().getWhite());
+        titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
+        listingPanel.add(titlePanel, BorderLayout.NORTH);
+    }
+    private void paintContent(String question, String answer) {
+        JPanel listItem = new JPanel();
 
+        listItem.setLayout(new BoxLayout(listItem, BoxLayout.Y_AXIS));
+        listItem.setBackground(Palette.instance().getWhite());
+
+        listItem.setMaximumSize(new Dimension(860, 500));
+        listItem.setBorder(new EmptyBorder(10,  30,  20,  0)); 
+
+        paintText(question, true, 19, 10, listItem);
+        
+        paintText(answer, false, 17, 0, listItem);
+
+        titlePanel.add(listItem);
+        
+    }
     protected void paintText(String text, boolean isBold, int textSize, int borderBottom, JPanel container) {
         int textWeight = isBold ? Font.BOLD : Font.PLAIN;
 
-        JLabel label = new JLabel("<html>" + text + "</html>");
+        JLabel label = new JLabel();
+        label.setText("<html>" + text + "</html>");
         label.setFont(new Font("Nunito Sans", textWeight, textSize));        
         label.setHorizontalAlignment(JLabel.LEFT);
         label.setVerticalAlignment(JLabel.CENTER);
         label.setForeground(Palette.instance().getBlack());
-        label.setBorder(new EmptyBorder(0,  10,  borderBottom, 0)); 
+        label.setBorder(new EmptyBorder(0,  0,  borderBottom, 0)); 
         
         container.add(label);
     }
 
-    
+    private void paintList() {
+        listingPanel = new JPanel();
+        listingPanel.setLayout(new BoxLayout(listingPanel, BoxLayout.Y_AXIS));
+        
+        listingPanel.setBackground(Palette.instance().getWhite());
+
+        this.setViewportView(listingPanel);
+        this.setPreferredSize(new Dimension(250, 320));
+        this.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        this.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        this.getVerticalScrollBar().setBackground(Palette.instance().getLightGray());
+        changeScrollPaneLook();
+
+        Border border = BorderFactory.createLineBorder(Palette.instance().getWhite(), 3);
+        this.setBorder(border);
+
+    }
     
     private void changeScrollPaneLook() {
         this.getVerticalScrollBar().setPreferredSize(new Dimension(8, 0));
