fffa9ddefbed38027c4b39223ad7596ec13efeeb
Sofia Marcano <soamaia03@gmail.com>
2024-02-12 18:51:29 -0400

    Check instructions is correctly saved ExamTest

--- src/test/models/ExamTest.java
+++ src/test/models/ExamTest.java
@@ -1,27 +1,56 @@
 package test.models;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.text.BadLocationException;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import main.models.Exam;
-
+import main.utils.Directory;
 public class ExamTest {
     private Exam exam;
+    private String instructions;
 
-    @Test (expected = NullPointerException.class)
-    public void testEmptyExam() {
-        exam = new Exam();
-        exam.getNameExam();
-        exam.getNameCourse();
-        exam.getTypeExam();
-        exam.getNameTeacher();
+    @BeforeAll
+    private void setUp() throws IOException {
+        String instructionPath = Directory.instance().getDirectoryExams()+File.separator+"Instructions.txt";
+        File file = new File(instructionPath);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        instructions = "";
+        boolean first = true;
+        while ((st = br.readLine()) != null) {
+            if (first) {
+                instructions = st;
+                first = false;
+            } else {
+                instructions = instructions + '\n' + st;
+            }
+        }
+        
     }
 
-    @Test
-    public void testExam() {
+    @Test 
+    public void testExam() throws IOException {
+        setUp();
         exam = new Exam();
-        Assertions.assertNotEquals(null, exam.getInstructions());
+        assertThrows(NullPointerException.class, () -> {exam.getNameExam();});
+        assertThrows(NullPointerException.class, () -> {exam.getNameCourse();});
+        assertNull(exam.getTypeExam());
+        assertThrows(NullPointerException.class, () -> {exam.getNameTeacher();});
+        Assertions.assertEquals(instructions, exam.getInstructions());
     }
 }
