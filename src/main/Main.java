54c091783ac33e4e645b56be0846164e8441ff76
Jesus <jesusecova73@gmail.com>
2024-02-12 18:05:13 -0400

    Connect views in Admin Flow

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,11 @@
 package main;
 
-import java.awt.Dimension;
-
-import javax.swing.Popup;
-
-import main.utils.Size;
-import main.utils.UserData;
-import main.views.components.PopUp;
 import main.views.pages.Frame;
 import main.views.pages.LogInView;
-import main.views.pages.NewExamPopup;
-import main.views.pages.NewExamView;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        UserData.instance().setUsername("Usuario");
-        UserData.instance().setPassword("Contrasenia");
-
-        // Frame.instance().setView(new LogInView());
-        Frame.instance().setView(new NewExamView());
-        PopUp.instance(Size.instance().getNewExamPopUpDimension()).setView(new NewExamPopup());
+        Frame.instance().setView(new LogInView());
     }
 }
