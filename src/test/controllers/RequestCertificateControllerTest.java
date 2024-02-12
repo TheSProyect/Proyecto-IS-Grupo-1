5e3f1cce0904860341993bd109b24129af66d8ce
ValCicco <valerria02@gmail.com>
2024-02-12 16:00:01 -0400

    .

--- src/test/controllers/RequestCertificateControllerTest.java
+++ src/test/controllers/RequestCertificateControllerTest.java
@@ -12,10 +12,8 @@ import java.nio.file.Paths;
 
 public class RequestCertificateControllerTest {
     RequestCertificateController controller = new RequestCertificateController();
-    Directory currentDirectory = Directory.instance();
     UserData currentUser = UserData.instance();
-    Certificate currentCertificate = new Certificate();
-    String course = "Java SE";
+    String course;
     String directory = System.getProperty("user.dir");
     
     //
@@ -25,25 +23,35 @@ public class RequestCertificateControllerTest {
 
     @Test
     public  void readStudentDataTest(){
-        //controller.searchFolderStudent(course);
-        directory = directory+File.separator+"src"+File.separator+"data"+File.separator+"Users"+File.separator+"Students";
-        directory = directory + File.separator + nameFolderStudent + File.separator + course + ".txt";
+        
+        directory = directory+File.separator+"src"+File.separator+"data"+File.separator+"Users"+File.separator+"Students"+File.separator + nameFolderStudent;
+        File searchedFolder = new File(directory);
+        if (searchedFolder.exists() && searchedFolder.isDirectory()) {
+            File[] files = searchedFolder.listFiles();
+            if (files != null) {
+            for (File fileExam : files) {
+                if(!(fileExam.getName().equals("Password.txt")) && !(fileExam.getName().startsWith(nameFolderStudent))){
+                    course=fileExam.getName();
+                } 
+                
+            }
+        }}
+
+        directory = directory + File.separator + course ;      
+        
         controller.readStudentData(directory);
         try (BufferedReader br = new BufferedReader(new FileReader(directory))) {
             // nombre del curso
-            Assert.assertEquals(br.readLine(), course);
+            Assert.assertEquals(br.readLine(), controller.getNameCourseController());
             //nombre del examen
-            //Assert.assertEquals(br.readLine(), course);
-            //Assert.assertEquals(br.readLine(), currentCertificate.getNameExam());
-            Assert.assertEquals(br.readLine(), controller.getNameExamController());
+            Assert.assertEquals(br.readLine(), course);
+            //Assert.assertEquals(br.readLine(), controller.getNameExamController());
             //resultado de examen
             Assert.assertEquals(Integer.parseInt(br.readLine()), controller.getResultAnswersController());
             //numero de preguntas
             Assert.assertEquals(Integer.parseInt(br.readLine()), controller.getQuestionsExamController());
-            //Assert.assertEquals(Integer.parseInt(br.readLine()), controller.getResultAnswersController());
             //nombre profe
             Assert.assertEquals(br.readLine(), controller.getNameTeacherController());
-            //currentCertificate.setNameTeacherCertificate(br.readLine());
 
             br.close();     
             } catch (IOException e) {
