5a4665eafe9c57e274199551df4a49ee62c4d834
Jesus <jesusecova73@gmail.com>
2024-02-12 20:03:11 -0400

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,12 +2,10 @@ package main;
 
 import main.views.pages.Frame;
 import main.views.pages.LogInView;
-import main.views.pages.RegisterUserView;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Frame.instance().setView(new LogInView());
-        Frame.instance().setView(new RegisterUserView());
+        Frame.instance().setView(new LogInView());
     }
 }
