ff855b49ba51e2aa6f7ee6441fce8411fc012a50
Lisangely Goncalves <lisangelyg@gmail.com>
2024-02-16 22:28:32 -0400

    add cec

--- src/main/controllers/CreateExamController.java
+++ src/main/controllers/CreateExamController.java
@@ -191,12 +191,14 @@ public class CreateExamController extends TemplateExam{
     }
     private void showQuestion(String directory, int readings, int counter, int stop){
         String line;
+        Boolean[] hasCode={false};
         List<String> code = new ArrayList<String>(), questionStatement = new ArrayList<String>();
         try (BufferedReader br = new BufferedReader(new FileReader(directory))) {
             currentExam.setQuestionsExam((readInformationQuestion(br, questionStatement, Integer.parseInt(br.readLine()))),(br.readLine()),counter);
-            if((line=br.readLine())!= "No"){
-                readInformationQuestion(br, code, Integer.parseInt(line));
+            if(!((line=br.readLine()).equals("No"))){
+                readCode(br, code, Integer.parseInt(line),hasCode);
                 currentExam.setCode(code,counter);
+                currentExam.setHasCode(hasCode[0], counter);
             }
             if(br.readLine().equals("Si")){
                 currentExam.setImageQuestion(true, counter);
